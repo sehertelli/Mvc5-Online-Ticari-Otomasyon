c144c0c5c2048e23dd44b69dbe40686d4440304d
Seher Telli <seherrtll@gmail.com>
2025-07-06 23:29:51 +0300

    Mvc5-ile-OnlineTicari-Otomasyon
    Asp.Net Mvc5 ile Online Ticari Otomasyon

---
+++ MvcOnlineTicariOtomasyon/MvcOnlineTicariOtomasyon/Controllers/CariController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcOnlineTicariOtomasyon.Models.Siniflar;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class CariController : Controller
+    {
+        // GET: Cari
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var degerler = c.Carilers.Where(x=>x.Durum==true).ToList();
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult YeniCari()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniCari(Cariler p)
+        {
+            p.Durum = true;
+            c.Carilers.Add(p);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult CariSil(int id)
+        {
+            var cr = c.Carilers.Find(id);
+            cr.Durum = false;
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult CariGetir(int id)
+        {
+            var cari = c.Carilers.Find(id);
+            return View("CariGetir",cari);
+        }
+        public ActionResult CariGuncelle(Cariler p)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("CariGetir");
+            }
+            var cr = c.Carilers.Find(p.Cariid);
+            cr.CariAd = p.CariAd;
+            cr.CariSoyad = p.CariSoyad;
+            cr.CariSehir = p.CariSehir;
+            cr.CariMail = p.CariMail;
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult MusteriSatis(int id)
+        {
+            var degeler = c.SatisHarekets.Where(x => x.Cariid == id).ToList();
+            var car = c.Carilers.Where(x => x.Cariid == id).Select(y => y.CariAd + " " + y.CariSoyad).FirstOrDefault();
+            ViewBag.cari = car;
+            return View(degeler);
+        }
+    }
+}
---
+++ MvcOnlineTicariOtomasyon/MvcOnlineTicariOtomasyon/Controllers/DepartmanController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcOnlineTicariOtomasyon.Models.Siniflar;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class DepartmanController : Controller
+    {
+        // GET: Departman
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var deger = c.Departmans.Where(x=>x.Durum==true).ToList();
+            return View(deger);
+        }
+        [Authorize(Roles ="A")]
+        [HttpGet]
+        public ActionResult DepartmanEkle()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult DepartmanEkle(Departman d)
+        {
+            c.Departmans.Add(d);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult DepartmanSil(int id)
+        {
+            var deg = c.Departmans.Find(id);
+            deg.Durum = false;
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult DepartmanGetir(int id)
+        {
+            var dep = c.Departmans.Find(id);
+            return View ("DepartmanGetir", dep);
+        }
+        public ActionResult DepartmanGuncelle(Departman p)
+        {
+            var dept = c.Departmans.Find(p.Departmanid);
+            dept.DepartmanAd = p.DepartmanAd;
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult DepartmanDetay(int id)
+        {
+            var degerler = c.Personels.Where(x => x.Departmanid == id).ToList();
+            var dpt = c.Departmans.Where(x => x.Departmanid == id).Select(y => y.DepartmanAd).FirstOrDefault();
+
+            ViewBag.d = dpt;
+            return View(degerler);
+        }
+        public ActionResult DepartmanPersonelSatis(int id)
+        {
+            var deger = c.SatisHarekets.Where(x => x.Personelid == id).ToList();
+            var per = c.Personels.Where(x => x.Personelid == id).Select(y => y.PersonelAd + " " + y.PersonelSoyad).FirstOrDefault();
+            ViewBag.dpers = per;
+            return View(deger);
+        }
+    }
+}
